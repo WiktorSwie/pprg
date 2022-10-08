f0782cc569b13e5126374d0734164ec3823ca511
WiktorSwie <115349384+WiktorSwie@users.noreply.github.com>
2022-10-08 12:44:09 +0200

    Update zad 1 4.cpp

--- zad 1 4.cpp
+++ zad 1 4.cpp
@@ -2,18 +2,21 @@
 using namespace std;
 int main()
 {
-    int x;
-    int y;
-    int z;
+    int number;
 
     cout << "Podaj liczbe calkowita:";
-    cin >> x;
-    cin >> y;
-    cin >> z;
-    if ( x > y) and (x > z)
-    {
-        /* code */
+    cin >> number;
+    if (number == 0){
+        cout << "liczba to 0";
     }
-    
+    else {
+        if (number % 2 ){
+            cout << "liczba jest nieparzysta";
+        }
+        else{
+            cout << "liczba jest parzysta";
+        }
+    }
+
 
 }
