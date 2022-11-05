a8d3a8693bce46313f01da8389eca8a97999e441
WiktorSwie <115349384+WiktorSwie@users.noreply.github.com>
2022-11-05 12:03:01 +0100

    Add files via upload

---
+++ zad do poprawienia 3.1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float x;
+    float y;
+    float suma;
+    float roznica;
+    float iloczyn;
+    float iloraz;
+    cout << "proszę wprowadzić x  i y" << endl;
+    cin >> x;
+    cin >> y;
+    suma=x+y;
+    suma = 
+    roznica=x-y;
+    iloczyn=x*y;
+    iloraz=x/y;
+    cout.precision(2);
+    cout << "suma:" << suma << endl;
+    cout << "różnica:" << roznica << endl;
+    cout << "iloczyn:" << iloczyn << endl;
+    cout << "iloraz:" << iloraz << endl;
+
+}
