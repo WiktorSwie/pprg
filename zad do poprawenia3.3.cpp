ff15dcbdb77d4ec5e980a8106b55c8a1fd540681
WiktorSwie <115349384+WiktorSwie@users.noreply.github.com>
2022-11-05 12:39:13 +0100

    Add files via upload

---
+++ zad do poprawenia3.3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int A;
+    int B;
+    int C;
+    cout << "postać równania kwadratowego równa się Ax+Bx^2+C" << endl;
+    cout << "proszę wprowadzić A,B,C" << endl;
+    cin >> A;
+    cin >> B;
+    cin >> C;
+    cout << "Postać twojego równania kwadratowego to:" << A << "x+" << B << "x^2+" << C;
+    
+}
